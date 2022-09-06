e939bb541a6abc7dcc465385da4549d2f8e1078c
jmsiro <jmsiro@gmail.com>
2022-09-06 13:12:09 -0300

    [NEW] Add Reply email fuction, add quantity of email in getAllEmails, fix readEmail JSON and attachments download

--- __init__.py
+++ __init__.py
@@ -61,8 +61,10 @@ if module == "sendEmail":
     attached_folder = GetParams("attached_folder")
     try:
         message = account.new_message()
-        list_mails = to_.split(",")
-        message.to.add(list_mails)
+        list_to = to_.split(",")
+        message.to.add(list_to)
+        list_cc = cc.split(",")
+        message.cc.add(list_cc)
         message.subject = subject
         message.body = body
         if attached_file:
@@ -74,6 +76,39 @@ if module == "sendEmail":
                 filenames.append(f)
             message.attachments.add(filenames)
         message.send()
+    except Exception as e:
+        print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
+        PrintException()
+        raise e
+
+if module == "replyEmail":
+    id_ = GetParams("id_")
+    body = GetParams("body")
+    attached_file = GetParams("attached_file")
+    attached_folder = GetParams("attached_folder")
+    read = GetParams("markasread")
+    
+    if not id_:
+        raise Exception("Missing Email ID...")
+    
+    try:
+        message = account.mailbox().get_message(id_)
+        reply = message.reply()
+        reply.body = body
+        
+        if attached_file:
+            reply.attachments.add(attached_file)
+        if attached_folder:
+            filenames = []
+            for f in os.listdir(attached_folder):
+                f = os.path.join(attached_folder, f)
+                filenames.append(f)
+            reply.attachments.add(filenames)
+        reply.send()
+        
+        if read == True:
+            message.mark_as_read()
+        
     except Exception as e:
         print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
         PrintException()
@@ -139,6 +174,7 @@ if module == "getFolders":
 if module == "getAllEmails":
     folder = GetParams("folder")
     res = GetParams("res")
+    limit = GetParams("limit")
     filter = GetParams("filtro") or GetParams("filter")
     
     OutlookWellKnowFolderNames = {
@@ -159,9 +195,14 @@ if module == "getAllEmails":
     if folder == "" or folder == None:
         folder = "Inbox"
     
+    if limit and limit != "":
+        limit = int(limit)
+    else:
+        limit = None
+    
     try:
         list_messages = account.mailbox().folder_constructor(parent=account.mailbox(), name=folder,
-                                                             folder_id=folder).get_messages(query=filter)
+                                                             folder_id=folder).get_messages(limit=limit, query=filter)
         list_object_id = []
         for message in list_messages:
             list_object_id.append(message.object_id)
@@ -174,22 +215,33 @@ if module == "getAllEmails":
 if module == "readEmail":
       
     att_folder = GetParams("att_folder")
+    download_att = GetParams("down")
     res = GetParams("res")
     id_ = GetParams("id_")
     read = GetParams("markasread")
     
+    if not id_:
+        raise Exception("Missing Email ID...")
+    
     try:
+        # It creates a message object and makes available attachments to be downloaded
         message = account.mailbox().get_message(id_, download_attachments=True)
         files = []
         for att in message.attachments:
-            files.append(att.name)
-            att.save(att_folder)
+            files.append(att)
+            if download_att:
+                if eval(download_att) == True:
+                    att.save(att_folder)
+        print(message.attachments)
         message_all = {
+            # Recipient object
             'sender': message.sender.address,
-            'cc': message.cc,
+            # Iterate over a Recipients object (List of Recipient objects) and parses each element into string
+            'cc': [str(rec) for rec in message.cc._recipients],
             'subject': message.subject,
-            'sent_time': message.sent,
-            'received': message.received,
+            # Parses elements datetime.datetime into string
+            'sent_time': message.sent.strftime('%d-%m-%Y %H:%M'),
+            'received': message.received.strftime('%d-%m-%Y %H:%M'),
             'body': BeautifulSoup(message.body, "html.parser").body.get_text(),
             'files': files
         }
@@ -197,6 +249,22 @@ if module == "readEmail":
             message.mark_as_read()
         
         SetVar(res, message_all)
+    except Exception as e:
+        SetVar(res, False)
+        print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
+        PrintException()
+        raise e
+
+if module == "listRootLists":
+      
+    res = GetParams("res")
+    
+    try:
+        sharepoint = account.sharepoint().get_root_site().get_default_document_library()
+        sharepoint_1 = account.sharepoint().get_root_site().get_lists()
+        sharepoint_2 = account.sharepoint().get_root_site().get_subsites()
+        
+        SetVar(res, sharepoint_2)
     except Exception as e:
         print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
         PrintException()
